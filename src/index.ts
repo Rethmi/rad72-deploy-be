f638993e9f58e85b1481863ad8512dea24fdd688
Rethmi Thennakoon <reththennakoon.edu@gmail.com>
2025-11-20 14:06:08 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const app = express()
 app.use(express.json())
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173" , "https://rad72-deploy-fe.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"] // optional
   })
 )
